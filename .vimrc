08b48f03726548254d0a17b65409bb66e3c56167
Paavo Pere <paavo@pere.fi>
2018-04-02 23:19:29 +0300

    lol

--- .vimrc
+++ .vimrc
@@ -1,3 +1,23 @@
 set nocompatible
-set backspace=2
+filetype indent plugin on
+syntax on
 set backspace=indent,eol,start
+set autoindent
+set laststatus=2
+set ignorecase
+set confirm
+set visualbell
+set mouse=a
+set cmdheight=2
+set incsearch 
+
+
+" https://jeffkreeftmeijer.com/vim-number/ 
+set number relativenumber
+augroup numbertoggle
+  autocmd!
+  autocmd BufEnter,FocusGained,InsertLeave * set relativenumber
+  autocmd BufLeave,FocusLost,InsertEnter   * set norelativenumber
+augroup END
+
+
--- ipython/profile_default/startup/sql_engine.py
+++ ipython/profile_default/startup/sql_engine.py
@@ -1,9 +1,9 @@
-def get_engine(user, password, host, port, connect_args=None):
+def get_engine(user, password, host, port, database='', connect_args=None):
     from sqlalchemy import create_engine
     if connect_args == None:
         connect_args = dict()
     protocol = 'mysql'
-    connect_string = '{}://{}:{}@{}:{}/'.format(protocol, user, password, host, port)
+    connect_string = '{}://{}:{}@{}:{}/{}'.format(protocol, user, password, host, port, database)
     return create_engine(connect_string, connect_args=connect_args)
 
 def engine_from_json_file(config_path):
