5dcb305b3da3f4e8640f93a6a6b04801dcf60242
Paavo Pere <paavo@pere.fi>
2018-07-30 23:59:21 +0300

    url params to sql engine

--- ipython/profile_default/startup/sql_engine.py
+++ ipython/profile_default/startup/sql_engine.py
@@ -1,18 +1,31 @@
-def get_engine(user, password, host, port, database='', connect_args=None):
+def get_engine(user, password, host, port, database='', connect_args=None, url_params=None):
     from sqlalchemy import create_engine
-    if connect_args == None:
+    if connect_args is None:
         connect_args = dict()
     protocol = 'mysql'
-    connect_string = '{}://{}:{}@{}:{}/{}'.format(protocol, user, password, host, port, database)
+    param_str = url_params_str(url_params)
+    connect_string = '{}://{}:{}@{}:{}/{}{}'.format(
+        protocol, user, password, host, port, database, param_str)
     return create_engine(connect_string, connect_args=connect_args)
 
-def engine_from_json_file(config_path):
+def url_params_str(url_params: {dict, None} = None) -> str:
+    """
+    >>> url_params_str(dict(a='lol', b=123))
+    '?a=lol&b=123'
+    >>> url_params_str()
+    ''
+    """
+    if not url_params:
+        return ''
+    return '?' + '&'.join('{0}={1}'.format(k, v) for k, v in url_params.items())
+
+def engine_from_json_file(config_path, **kwargs):
     import json
     with open(config_path) as f:
         config = json.load(f)
-    return get_engine(**config)
+    return get_engine(**config, **kwargs)
 
-def paavosql():
-    return engine_from_json_file('/home/paavo/.mysql/reportingdb.json')
+def paavosql(**kwargs):
+    return engine_from_json_file('/home/paavo/.mysql/reportingdb.json', **kwargs)
 
 dbe = db_engine = paavosql
