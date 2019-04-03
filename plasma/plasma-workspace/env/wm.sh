8374205891a320132e2cb749a7a459023e80bf4e
Paavo Pere <paavo@pere.fi>
2019-04-03 23:35:44 +0300

    separate script to start kde with i3

--- plasma/plasma-workspace/env/wm.sh
+++ plasma/plasma-workspace/env/wm.sh
@@ -1,3 +1,8 @@
-export KDEWM=/usr/bin/i3
-compton & --config ~/.config/compton/compton.conf
+if [ "$KDEWM" = "/usr/bin/i3" ]
+then
+    export LELX="in i3"
+    compton & --config ~/.config/compton/compton.conf
+else
+    export LELX="in kwin"
+fi
 
---
+++ scripts/startkde-i3.sh
@@ -0,0 +1,2 @@
+export KDEWM=/usr/bin/i3
+/usr/bin/startkde
